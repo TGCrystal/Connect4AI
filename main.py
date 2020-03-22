485bc6fc180b463c233c29a5b4a64f6fd7c02b78
Trevor <TGCrystal@users.noreply.github.com>
2020-03-22 13:55:19 -0400

    added depth so moved can be calculated in realistic timeframe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import random
 
 
 # Enable and disable print from https://stackoverflow.com/questions/8391411/suppress-calls-to-print-python
@@ -13,16 +14,17 @@ def enablePrint():
     sys.stdout = sys.__stdout__
 
 
-def minAI(gameBoard, aiPiece, alpha=float('-inf'), beta=float('inf')):
+def minAI(gameBoard, aiPiece, maxDepth, currentDepth=0, alpha=float('-inf'), beta=float('inf')):
     if aiPiece == 'r':
         humanPiece = 'b'
     else:
         humanPiece = 'r'
-    if gameBoard.isFull():
+    if gameBoard.isFull() or currentDepth == maxDepth:
         return 0, None
     bestScore = 0
-    bestAction = None
+    bestActions = None
     actions = gameBoard.getValidMoves()
+    # gameBoard.print()
     for action in actions:
         isWin, row = gameBoard.addPiece(humanPiece, action)
         gameBoard.resetPosition(row, action)
@@ -30,25 +32,27 @@ def minAI(gameBoard, aiPiece, alpha=float('-inf'), beta=float('inf')):
             return -1, action
     for action in actions:
         row = gameBoard.addPiece(humanPiece, action)[1]
-        successorScore = maxAI(gameBoard, aiPiece, alpha, beta)[0]
+        successorScore = maxAI(gameBoard, aiPiece, maxDepth, currentDepth+1, alpha, beta)[0]
         gameBoard.resetPosition(row, action)  # gameBoard isn't deep copied, so must be preserved
-        if bestAction is None or successorScore < bestScore:
+        if bestActions is None or successorScore < bestScore:
             if successorScore < beta:
                 beta = successorScore
             bestScore = successorScore
-            bestAction = action
+            bestActions = [action]
             if alpha > beta:
-                return bestScore, bestAction
-    if bestAction is None:  # "if" at start of method should ensure this doesn't happen
+                return bestScore, action
+        elif successorScore == bestScore:
+            bestActions.append(action)
+    if bestActions is None:  # "if" at start of method should ensure this doesn't happen
         return 0, None
-    return bestScore, bestAction
+    return bestScore, random.choice(bestActions)
 
 
-def maxAI(gameBoard, aiPiece, alpha=float('-inf'), beta=float('inf')):
-    if gameBoard.isFull():
+def maxAI(gameBoard, aiPiece, maxDepth, currentDepth=0, alpha=float('-inf'), beta=float('inf')):
+    if gameBoard.isFull() or currentDepth == maxDepth:
         return 0, None
     bestScore = 0
-    bestAction = None
+    bestActions = None
     actions = gameBoard.getValidMoves()
     for action in actions:
         isWin, row = gameBoard.addPiece(aiPiece, action)
@@ -57,18 +61,20 @@ def maxAI(gameBoard, aiPiece, alpha=float('-inf'), beta=float('inf')):
             return 1, action
     for action in actions:
         row = gameBoard.addPiece(aiPiece, action)[1]
-        successorScore = minAI(gameBoard, aiPiece, alpha, beta)[0]
+        successorScore = minAI(gameBoard, aiPiece, maxDepth, currentDepth+1, alpha, beta)[0]
         gameBoard.resetPosition(row, action)  # gameBoard isn't deep copied, so must be preserved
-        if bestAction is None or successorScore > bestScore:
+        if bestActions is None or successorScore > bestScore:
             if successorScore > alpha:
                 alpha = successorScore
             bestScore = successorScore
-            bestAction = action
+            bestActions = [action]
             if alpha > beta:
-                return bestScore, bestAction
-    if bestAction is None:  # "if" at start of method should ensure this doesn't happen
+                return bestScore, action
+        elif successorScore == bestScore:
+            bestActions.append(action)
+    if bestActions is None:  # "if" at start of method should ensure this doesn't happen
         return 0, None
-    return bestScore, bestAction
+    return bestScore, random.choice(bestActions)
 
 
 def main():
@@ -92,13 +98,14 @@ def main():
             if gameBoard.isValidColumn(column):
                 gameBoard.addPiece(humanPiece, column)
                 break
+        blockPrint()
+        computerAction = maxAI(gameBoard, aiPiece, 6)[1]
+        gameBoard.addPiece(aiPiece, computerAction)
+        enablePrint()
     else:
         humanPiece = 'b'
         aiPiece = 'r'
-    blockPrint()
-    computerAction = maxAI(gameBoard, aiPiece)
-    gameBoard.addPiece(aiPiece, computerAction)
-    enablePrint()
+        gameBoard.addPiece(aiPiece, 3)
     while gameBoard.winner is None and not gameBoard.isFull():
         gameBoard.print()
         print("Select a number associated with the column you want to choose")
@@ -111,7 +118,7 @@ def main():
         if gameBoard.winner is not None or gameBoard.isFull():
             break
         blockPrint()
-        computerAction = maxAI(gameBoard, aiPiece)
+        computerAction = maxAI(gameBoard, aiPiece, 7)[1]
         gameBoard.addPiece(aiPiece, computerAction)
         enablePrint()
     gameBoard.print()
